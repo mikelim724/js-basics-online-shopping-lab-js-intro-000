2a237618ad92d175d06a66740a55711aff6fdf39
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-21 02:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber===undefined) {
-    return "Sorry we don't have a credit card on file for you."
+    return "Sorry, we don't have a credit card on file for you."
   }
   else {
     var sum= total();
